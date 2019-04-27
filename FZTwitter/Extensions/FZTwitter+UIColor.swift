552a8f0434e03bea7efb247430c24bea2e1b8e84
Leonardo Domingues <leodmgs@gmail.com>
2019-04-26 23:59:09 -0300

    Add custom colors

--- FZTwitter/Extensions/FZTwitter+UIColor.swift
+++ FZTwitter/Extensions/FZTwitter+UIColor.swift
@@ -13,4 +13,12 @@ extension UIColor {
     
     static let fzBlue = UIColor(red: 0.11, green: 0.63, blue: 0.95, alpha: 1)
     
+    static let fzRed = UIColor(red: 0.92, green: 0.25, blue: 0.20, alpha: 1)
+    
+    static let fzLightGray = UIColor(red: 0.67, green: 0.72, blue: 0.76, alpha: 1)
+    
+    static let fzDarkGray = UIColor(red: 0.40, green: 0.47, blue: 0.52, alpha: 1)
+    
+    static let fzGreen = UIColor(red: 0, green: 0.36, blue: 0.16, alpha: 1)
+    
 }
